0354e32b95ef3f166b814d887a91492fbef52a15
MarvinJWendt <git@marvinjwendt.com>
2022-10-09 03:05:12 +0200

    updated docs

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 /*
 Package list implements a generic list. It is a wrapper around a slice and has multiple useful methods.
+It can be used to develop in a functional style, but it is not required. The main purpose of this package is, to make working with slices easier.
+The package supports sorting, shuffling, filtering, mapping, appending, prepending, removing, inserting and more.
 */
 package list
